ead515c00ab591514e0be259a20d0fea65e34b02
MikeielRP <72733304@continental.edu.pe>
2025-04-23 11:30:23 -0500

    Prueba 2 ok

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -3,4 +3,7 @@ class conjunto:
         self.conjunto=conjunto
 
     def promedio ( self ):
-        return None
+        if len(self.conjunto)==1:
+            return (self.conjunto[0])
+        else:
+            return None
